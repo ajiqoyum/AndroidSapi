3b75c2d24ad77dc8452135f2c32c513d61ace605
ajiqoyum <ajiqoyum@gmail.com>
2022-07-31 21:23:41 +0700

    Fix minor bug
    Hapus finish() dari listener

--- app/src/main/java/com/example/v01/Main/User.kt
+++ app/src/main/java/com/example/v01/Main/User.kt
@@ -23,7 +23,6 @@ class User : AppCompatActivity() {
         detaile.setOnClickListener {
             val intent = Intent(this, UserDetails::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
-            finish()
             startActivity(intent)
         }
     }
